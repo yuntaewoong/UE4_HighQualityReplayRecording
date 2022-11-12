68ba42031bc8e259ff6fcb46dc015610ec896765
DESKTOP-R27HG89\Yoon <15ywt@naver.com>
2022-11-12 17:23:09 +0900

    Add Feature Finding Replay File Names

--- Source/PathTracingRecord/RecordingManager.cpp
+++ Source/PathTracingRecord/RecordingManager.cpp
@@ -18,5 +18,24 @@ void ARecordingManager::SetIsRecording(bool isRecording)
 	m_IsRecording = isRecording;
 }
 
+TArray<FString> ARecordingManager::GetReplayFileNames()
+{
+    TArray<FString> fileNames= TArray<FString>();//결과 FString배열
+	IPlatformFile& FileManager = FPlatformFileManager::Get().GetPlatformFile();//언리얼 file system레퍼런스
+    FString replayDirectory = TEXT("");
+    {//replay 디렉토리는 프로젝트directory/Saved/Demos/
+        replayDirectory = FPaths::ProjectSavedDir();
+        replayDirectory.Append(TEXT("Demos/"));
+        if (!FileManager.DirectoryExists(*replayDirectory))//디렉토리 존재 체크
+            UE_LOG(LogTemp, Error, TEXT("FilePaths: %s does not exist"), *replayDirectory);
+    } 
+    DirectoryVisitor visitor(&fileNames);//커스텀 visitor
+    if (!FileManager.IterateDirectory(*replayDirectory, visitor))//디렉토리 순회
+        UE_LOG(LogTemp, Warning, TEXT("FilePaths: Directory did not exist or visitor returned false"));
+        
+    return fileNames;
+
+}
+
 
 
--- Source/PathTracingRecord/RecordingManager.h
+++ Source/PathTracingRecord/RecordingManager.h
@@ -24,7 +24,28 @@ public:
 
 	UFUNCTION(BlueprintCallable)
 		void SetIsRecording(bool isRecording);
+
+	UFUNCTION(BlueprintCallable)
+		TArray<FString> GetReplayFileNames();// 프로젝트경로/Saved/Demos에 저장된 .replay파일들의 이름배열을 리턴
 private:
+    struct DirectoryVisitor : public IPlatformFile::FDirectoryVisitor
+    {
+		DirectoryVisitor(TArray<FString>* pReplayFileNames)
+		{
+			m_pReplayFileNames = pReplayFileNames;
+		}
+		TArray<FString>* m_pReplayFileNames;//replay파일들의 이름배열 포인터
+        bool Visit(const TCHAR* FilenameOrDirectory, bool bIsDirectory) override//디렉토리를 순회할때마다 실행되는 함수
+        {
+			if (bIsDirectory)
+				return true;//디렉토리인 경우 아무것도 안함
+			FString replayName = FPaths::GetPathLeaf(FilenameOrDirectory);//경로의 최하위 이름=파일이름
+			replayName = replayName.Replace(TEXT(".replay"), TEXT(""));//.replay포맷명 삭제
+			m_pReplayFileNames->Add(replayName);//배열에 추가
+            return true;
+        }
+    };
 	bool m_IsRecording = false;//현재 recording중인지를 나타냄
 
 };
+
